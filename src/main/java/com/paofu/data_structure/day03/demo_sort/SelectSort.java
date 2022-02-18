5dfcaa81f1045683828a3618c690683a271a6800
泡芙和树 <2550206744@qq.com>
2022-02-18 17:12:32 +0800

    demo

--- src/main/java/com/paofu/data_structure/day03/demo_sort/SelectSort.java
+++ src/main/java/com/paofu/data_structure/day03/demo_sort/SelectSort.java
@@ -1,5 +1,7 @@
 package com.paofu.data_structure.day03.demo_sort;
 
+import java.util.Arrays;
+
 /**
  * @Description:
  * @Author: 泡芙和树
@@ -9,5 +11,87 @@ public class SelectSort {
 
     public static void main(String[] args) {
         int[] array = {10, 34, 19, 1, 46};
+//        selectSortStep(array);
+        selectSort(array);
+    }
+
+    /**
+     * [1, 34, 19, 10, 46]
+     * [1, 10, 19, 34, 46]
+     * [1, 10, 19, 34, 46]
+     * [1, 10, 19, 34, 46]
+     * @param array
+     */
+    public static void selectSortStep(int[] array) {
+        // 第一步
+        // 首先假设第一个数据是最小值
+        int min = array[0];
+        int minIndex = 0;
+        int temp;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                minIndex = i;
+            }
+        }
+        temp = array[0];
+        array[0] = array[minIndex];
+        array[minIndex] = temp;
+        System.out.println(Arrays.toString(array));
+        // 第二步
+        min = array[1];
+        minIndex = 1;
+        for (int i = 2; i < array.length; i++) {
+            if (array[i] < min) {
+                minIndex = i;
+            }
+        }
+        temp = array[1];
+        array[1] = array[minIndex];
+        array[minIndex] = temp;
+        System.out.println(Arrays.toString(array));
+        // 第三步
+        min = array[2];
+        minIndex = 2;
+        for (int i = 3; i < array.length; i++) {
+            if (array[i] < min) {
+                minIndex = i;
+            }
+        }
+        temp = array[2];
+        array[2] = array[minIndex];
+        array[minIndex] = temp;
+        System.out.println(Arrays.toString(array));
+        // 第四步
+        min = array[3];
+        minIndex = 3;
+        for (int i = 4; i < array.length; i++) {
+            if (array[i] < min) {
+                minIndex = i;
+            }
+        }
+        temp = array[3];
+        array[3] = array[minIndex];
+        array[minIndex] = temp;
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void selectSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = array[i];
+            int minIndex = i;
+            int temp;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                array[minIndex] = array[i];
+                array[i] = min;
+            }
+            System.out.println("第" + (i + 1) + "次排序后的结果为：" + Arrays.toString(array));
+        }
+
     }
 }
--- src/main/java/com/paofu/data_structure/demo/Test1.java
+++ src/main/java/com/paofu/data_structure/demo/Test1.java
@@ -1,9 +1,18 @@
 package com.paofu.data_structure.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Description:
  * @Author: 泡芙和树
  * @Date: 2022/1/24 17:29
  */
 public class Test1 {
+
+    private static final List<String> LIST = Arrays.asList("1", "2", "3", "4", "5");
+
+    public static void main(String[] args) {
+        System.out.println(LIST.contains("2"));
+    }
 }
