b3a8e0296fe645fec72b8abc89c7b7660772b202
泡芙和树 <2550206744@qq.com>
2022-02-25 16:33:38 +0800

    提交demo

---
+++ src/main/java/com/paofu/data_structure/day05/graph/GraphTest.java
@@ -0,0 +1,20 @@
+package com.paofu.data_structure.day05.graph;
+
+/**
+ * @Description:
+ * @Author: 泡芙和树
+ * @Date: 2022/2/25 16:00
+ */
+public class GraphTest {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class GraphDemo {
+
+    String[] vertexs;
+
+
+}
